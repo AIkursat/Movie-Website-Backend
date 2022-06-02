9bcc9fe57dba1ed8625698eb00f4028238999db3
AIKursat <aynurkursat1999@hotmail.com>
2022-06-02 23:12:16 +0300

    .

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -12,5 +12,8 @@ func (app *application) routes() *httprouter.Router{
 
 	router.HandlerFunc(http.MethodGet, "/status", app.StatusHandler)
 	
+	router.HandlerFunc(http.MethodGet, "/v1/movie/:id", app.getOneMovie )
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.getAllMovies)
+
 	return router
 }
---
+++ cmd/api/utilities.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, wrap string) error {
+	wrapper := make(map[string]interface{})
+
+	wrapper[wrap] = data
+
+	js, err := json.Marshal(wrapper)
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
+}
+
+func(app *application) errorJSON(w http.ResponseWriter, err error){
+	type JsonError struct{
+		message string `json:"message"`
+	}
+
+	theError := JsonError{
+		message: err.Error(),
+	}	
+	app.writeJSON(w, http.StatusBadRequest, theError, "error")
+}
