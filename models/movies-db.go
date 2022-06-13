885f9c0d8057d89ec21e2882db1d78aa18008e12
AIKursat <aynurkursat1999@hotmail.com>
2022-06-13 22:09:42 +0300

    update movie

--- models/movies-db.go
+++ models/movies-db.go
@@ -205,3 +205,30 @@ func (m *DBModel) InsertMovie(movie Movie) error {
 
 	return nil
 }
+
+func (m *DBModel) Updatemovie(movie Movie) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	stmt := `update movies set title $1, description = $2, year = $3, release_date = $4, runtime = $5, rating = $6, mpaa_rating =$7,
+				updated_at = $8 where id = $9`
+
+	_, err := m.DB.ExecContext(ctx, stmt,
+		movie.Title,
+		movie.Description,
+		movie.Year,
+		movie.ReleaseDate,
+		movie.Runtime,
+		movie.Rating,
+		movie.MPAARating,
+		movie.UpdatedAt,
+		movie.ID,
+	)
+
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
